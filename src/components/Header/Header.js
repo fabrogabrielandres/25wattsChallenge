0b870320bfd76553204c2826d2181b0adff8256a
Gabriel Fabro <84206590+fabrogabrielandres@users.noreply.github.com>
2021-11-08 23:39:40 -0600

    Fetching Header ok

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import "react-responsive-carousel/lib/styles/carousel.min.css"
 import { HeaderWrapper } from './styles'
+import { Carousel } from 'react-responsive-carousel'
+import axios from 'axios'
+import "./slide.css"
+
+
+
 
 export const Header = () => {
+
+    const [data, setData] = useState([{}])
+
+    useEffect(() => {
+        fetching()
+    }, [])
+
+    const fetching = async () => {
+        try {
+            const responce = await axios.get("http://localhost:5000/slider")
+            setData(responce.data);
+            console.log(responce.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
     return (
         <HeaderWrapper>
-            Header
+            <Carousel className="main-slide" showArrows={true} onChange={onchange} onClickItem={onclick} >
+                {
+                    data.map((dat,idx) => {
+                        return (
+                            <div key={idx}>
+                                <img src={dat.url} alt="" />
+                            </div>
+                        )
+                    })
+                }
+            </Carousel>
         </HeaderWrapper>
     )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,10 +1,7 @@
 import styled from "styled-components";
-import image from "../../assets/slide1-background.png"
 
-
-
-export const HeaderWrapper = styled.div`
-width: 100%;
-height: 100%;
-background-image: url(${image});
+export const HeaderWrapper = styled.div`  
+  width: 100%;
+  height: 100%;
+  
 `

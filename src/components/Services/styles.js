4a4b29132de0949fdc2cefec1db08c75fc3322d8
Gabriel Fabro <84206590+fabrogabrielandres@users.noreply.github.com>
2021-11-10 10:39:00 -0600

    fixed services

--- src/components/Services/styles.js
+++ src/components/Services/styles.js
@@ -4,16 +4,17 @@ import styled from "styled-components";
 
 export const ServicesWrapper = styled.div`
     width: 100%;
+    height: 100%;
     min-height: 552px;
-    /* display: flex; */
-    /* flex-direction: column; */
+    display: flex;
+    flex-direction: column;
     background-color:var(--unnamed-color-dce1da);
     
     `
 
 
 export const TitleService = styled.div`
-        padding-top: 104px;
+        margin-top: 104px;
     h1{
         font: var(--unnamed-font-style-normal) normal var(--unnamed-font-weight-600) var(--unnamed-font-size-39)/var(--unnamed-line-spacing-45) var(--unnamed-font-family-poppins);
         letter-spacing: var(--unnamed-character-spacing-0-55);
@@ -24,12 +25,12 @@ export const TitleService = styled.div`
         color: #343434;
         opacity: 1;
     }
-    
-    `
+`
+
 export const SubTitleService = styled.div`    
-    padding: 17px 295px;
+    margin: 17px 295px;
     @media (max-width:1100px){
-        padding: 17px 80px;
+        margin: 17px 80px;
     }
     h2{
         font: var(--unnamed-font-style-normal) normal var(--unnamed-font-weight-normal) var(--unnamed-font-size-16)/var(--unnamed-line-spacing-28) var(--unnamed-font-family-roboto);
@@ -45,10 +46,10 @@ export const SubTitleService = styled.div`
 
 export const CardService = styled.div`
     max-width: 206px;
-    max-height: 140px;
+    min-height: 142px;
     width: 100%;
     height: 100%;
-    padding-top: 80px;
+    margin-top: 80px;
 `
 
 export const IconsService = styled.div`
@@ -59,12 +60,10 @@ export const IconsService = styled.div`
         margin: 0px 81px;
     }
     h1{
-        padding-top: 37px;
+        margin-top: 37px;
         text-align: center;
         font-size: 1.7rem;
         margin-bottom: 20px;
-
-
     }
 
 `
